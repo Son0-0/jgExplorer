271b0e29afc7bd841e577939c50ba41036c94015
Son0-0 <jsmith032211@gmail.com>
2022-03-29 13:54:28 +0900

    commit

--- main.py
+++ main.py
@@ -1,14 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for
 from flask import request
 import pymongo
+import pymdb
 import bcrypt
 
 app = Flask(__name__)
-SECRET_KEY = "THISISSECRETKEY"
-
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-  return render_template('login.html')
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
@@ -17,16 +13,11 @@ def home():
     uid = data.get('uid')
     upw = data.get('upw')
     
-    byte_plain = upw.encode('UTF-8')
-    pw_hash = bcrypt.hashpw(byte_plain, bcrypt.gensalt()).hex()
+    plain_text = upw.encode('UTF-8')
+    pw_hash = bcrypt.hashpw(plain_text, bcrypt.gensalt()).hex() # DB에 저장될 값
     
+    pymdb.register(uid, pw_hash)
     print(uid, upw, pw_hash)
-    
-    temp_pw = "123"
-    tplain = temp_pw.encode('UTF-8')
-    dbdata = "2432622431322475726d6f316c526d44353364556955473876466c532e796f394c2f34486f4279677a75475a6e613064635373354634793963366747"
-    origin_pw = bytes.fromhex(dbdata) # DB에 저장되어 있는 값
-    print(bcrypt.checkpw(tplain, origin_pw))
     
   return render_template('login.html')
  
@@ -42,3 +33,8 @@ def register():
 
 if __name__ == '__main__':
    app.run('0.0.0.0', port=7800, debug=True)
+   
+@app.route('/mypage', methods=['GET', 'POST'])
+def mypage():
+  
+  return render_template('mypage.html')
